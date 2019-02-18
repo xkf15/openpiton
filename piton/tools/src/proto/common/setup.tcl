329adce2bd263126e2b7aeffa85c4b4b445fa21e
Jonathan Balkind <jbalkind@princeton.edu>
2019-02-18 14:06:30 -0500

    Changes to FPGA flow to use FPGA bootrom with OpenPiton SDHC

--- piton/tools/src/proto/common/setup.tcl
+++ piton/tools/src/proto/common/setup.tcl
@@ -119,10 +119,11 @@ set ALL_INCLUDE_FILES [pyhp_preprocess ${ALL_INCLUDE_FILES}]
 if  {[info exists ::env(PITON_ARIANE)]} {
   puts "INFO: compiling DTS for ariane..."
   set TMP [pwd]
-  cd $::env(ARIANE_ROOT)/openpiton/bootrom 
+  cd $::env(ARIANE_ROOT)/fpga/src/bootrom 
   # Note: dd dumps info to stderr that we do not want to interpret
   # otherwise this command fails...
-  exec make 2> /dev/null
+  exec make clean 2> /dev/null
+  exec make all 2> /dev/null
   cd $TMP
   puts "INFO: done"
 }
