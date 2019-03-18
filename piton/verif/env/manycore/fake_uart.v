57e79b13370715f1d0895adfe7139f23c8b09fb6
Jonathan Balkind <jbalkind@cs.princeton.edu>
2019-03-18 10:16:04 -0700

    Updates to simulate with verilator. Only prints 'he' of 'hello world' so far though.

--- piton/verif/env/manycore/fake_uart.v
+++ piton/verif/env/manycore/fake_uart.v
@@ -52,6 +52,7 @@ end
 
 always @(posedge clk) begin
     if (m_axi_awvalid & m_axi_wvalid & (m_axi_awaddr[2:0] == 3'b0)) begin
+        $display("FAKE UART OUTPUT: %c", m_axi_wdata[7:0]);
         $fwrite(file, "%c", m_axi_wdata[7:0]);
         $fflush(file);
     end
